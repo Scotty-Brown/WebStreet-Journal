74c0e0a6c897ffa942bf637168d338c84645db79
Scotty Brown <scottybrown98@gmail.com>
2023-11-01 16:10:22 -0500

    Add fetch all to App comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import { Router } from 'react-router-dom';
+import { fetchArticles } from './apiCalls';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [articles, setArticles] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    fetchArticles()
+    .then(data => setArticles(data.articles))
+    .catch(error => setError(error.message))
+  }, [])
+
   return (
     <div className='App'>
-      <Router>
+      {/* <Router>
 
-      </Router>
+      </Router> */}
     </div>
   )
 }
