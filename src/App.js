ab123585e92477818fd2b8f4dfc0af6c14cb190a
Scotty Brown <scottybrown98@gmail.com>
2023-11-01 15:45:56 -0500

    Delete unused boilerplate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Router } from 'react-router-dom';
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <Router>
-    
+
       </Router>
     </div>
   )
