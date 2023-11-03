a6b767fe9d07ac100738f69c184c872379571cb3
Scotty Brown <scottybrown98@gmail.com>
2023-11-03 13:33:12 -0500

    Add testing for bookmarking feature

--- cypress/e2e/bookmarks-spec.cy.js
+++ cypress/e2e/bookmarks-spec.cy.js
@@ -1,5 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Bookmarks', () => {
+
+  beforeEach(() => {
+    // cy.intercept('GET', 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=006734d12ac947799b7ba8fb0af145fd', {
+    //   statusCode: 200,
+    //   fixture: 'homeSpecData'
+    // }).as('homeSpecData')
+    cy.visit('http://localhost:3000/')
+    // .wait('@homeSpecData')
   })
+
+
+  it('Should be able to add news articles to bookmarks', () => {
+    cy.get('.bookmark-btn').first().click()
+    cy.get('.bookmark-btn').last().click()
+    cy.get('[href="/bookmarks"]').click()
+    cy.url('should', )
+    cy.get('.article-container').children().should('have.length', 2)
+    cy.get('.article-preview-card').first().find('h2').should('contain', 'Caroline Ellison Offered to Step Down, Bankman-Fried Says')
+    cy.get('.article-preview-card').last().find('h2').should('contain', 'UAW, Stellantis Reach Tentative Agreement on New Four-Year Labor Contract')
+  })
+
+  it('Should be able to remove article from bookmarks', () => {
+    cy.get('.bookmark-btn').first().click()
+    cy.get('[href="/bookmarks"]').click()
+    cy.url().should('eq', 'http://localhost:3000/bookmarks')
+    cy.get('.article-container').children().should('have.length', 1)
+    cy.get('.bookmark-btn').first().click()
+    cy.get('.empty-bookmarks').should('exist')
+    cy.get('p').should('contain', 'Looks like you haven\'t bookmarked any articles yet!')
+  })
+
 })
