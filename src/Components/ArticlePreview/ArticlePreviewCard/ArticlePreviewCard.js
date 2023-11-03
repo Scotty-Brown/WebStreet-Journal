56d438fd004283a6eb4fc31e0f0a4942a5a20184
Scotty Brown <scottybrown98@gmail.com>
2023-11-03 03:38:14 -0500

    Add local storage to store bookmarked articles

--- src/Components/ArticlePreview/ArticlePreviewCard/ArticlePreviewCard.js
+++ src/Components/ArticlePreview/ArticlePreviewCard/ArticlePreviewCard.js
@@ -3,15 +3,22 @@ import './ArticlePreviewCard.css'
 import { BsBookmarkCheckFill, BsBookmarkCheck } from "react-icons/bs";
 
 const ArticlePreviewCard = ({article, index}) => {
-    console.log(index)
+
+    const bookmarkArticle = () => {
+        const currentBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+        const updatedBookmarks = currentBookmarks.concat(article)
+        localStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks));
+    }
+
     return (
         <div className='article-preview-card'>
             <img className='article-preview-image' src={article.urlToImage} alt=''></img>
             <div className='article-details'>
+                <button onClick={bookmarkArticle}>Bookmark</button>
                 <h2>{article.title}</h2>
                 <p>{article.author}</p>
                 <p>{article.description}</p>
-                <Link to={`/${index}`}>click me</Link>
+                <Link to={`/${index}`}>Read More...</Link>
             </div>
         </div>
     )
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className='header'>
             <h1>WebStreet Journal</h1>
             <NavLink to={'/'}>Home</NavLink>
-            <NavLink>Read Later</NavLink>
+            <NavLink>Bookmarks</NavLink>
         </div>
     )
 }
